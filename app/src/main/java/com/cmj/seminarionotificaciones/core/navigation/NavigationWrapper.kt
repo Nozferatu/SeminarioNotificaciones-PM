444d94b3c2ad7b8fb14360fc63fc886f3c34c13b
Nozferatu <sparrowhawk2005@gmail.com>
2025-01-17 16:20:18 +0100

    Intento de Intent

--- app/src/main/java/com/cmj/seminarionotificaciones/core/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/cmj/seminarionotificaciones/core/navigation/NavigationWrapper.kt
@@ -5,11 +5,18 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.cmj.seminarionotificaciones.core.navigation.screens.Ejercicio1
 
 @Composable
-fun NavigationWrapper() {
+fun NavigationWrapper(actividadInicial: String) {
     val contexto = LocalContext.current
     val navController = rememberNavController()
+
+    if(actividadInicial.isNotEmpty()){
+        when(actividadInicial){
+            "Ejercicio" -> {navController.navigate(Ejercicio1)}
+        }
+    }
 
     NavHost(navController = navController, startDestination = Inicio){
         composable<Inicio>{
