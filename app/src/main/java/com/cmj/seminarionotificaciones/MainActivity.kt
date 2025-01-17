e9a84787de01c885aaa847285cd2251d4150fce1
Nozferatu <sparrowhawk2005@gmail.com>
2025-01-17 16:19:58 +0100

    Quitados imports innecesarios e intento de Intent

--- app/src/main/java/com/cmj/seminarionotificaciones/MainActivity.kt
+++ app/src/main/java/com/cmj/seminarionotificaciones/MainActivity.kt
@@ -1,38 +1,20 @@
 package com.cmj.seminarionotificaciones
 
-import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import com.cmj.seminarionotificaciones.MainActivity.Companion.CHANNEL_ID
-import com.cmj.seminarionotificaciones.MainActivity.Companion.id
 import com.cmj.seminarionotificaciones.core.navigation.NavigationWrapper
 import com.cmj.seminarionotificaciones.ui.theme.SeminarioNotificacionesPMTheme
 import java.util.concurrent.atomic.AtomicInteger
 
 class MainActivity : ComponentActivity() {
+    private lateinit var actividadIntent: String
+
     companion object {
         val APP_ID = "com.cmj.seminarionotificaciones"
         val CHANNEL_ID = "${APP_ID}_69"
@@ -45,9 +27,11 @@ class MainActivity : ComponentActivity() {
 
         createNotificationChannel()
 
+        actividadIntent = intent.getStringExtra("actividad") ?: ""
+
         setContent {
             SeminarioNotificacionesPMTheme {
-                NavigationWrapper()
+                NavigationWrapper(actividadIntent)
             }
         }
     }
